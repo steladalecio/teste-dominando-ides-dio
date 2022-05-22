0a7d6dc68b85b9ab471bf6da8737ed57aec0e478
StelaDalecio <steladalecio@hotmail.com>
2022-05-22 15:20:44 -0400

    First Commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+         Gato gato = new Gato();
+        System.out.println(gato);
+
+/*        int a = 10 ;
+        int b = 5 ;
+        System.out.println("Hello world " +  (a+b));*/
+    }
+}
